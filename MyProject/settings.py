14c0e244457f0a4f745cd757c26e335cc8e934d4
Vidhipal2003 <vidhipal857244@gmail.com>
2026-06-28 00:09:31 +0530

    Allow Render host via ALLOWED_HOSTS env var

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['skillsphere-aenu.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "skillsphere-aenu.onrender.com,127.0.0.1,localhost",
+).split(",")
 
 
 # Application definition
